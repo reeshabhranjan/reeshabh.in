2bd5b7d318e23e584fc73c9552b9036a29ce593f
President <president@oxfordfls.org>
2024-02-17 04:52:45 +0530

    Minor changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,15 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Reeshabh Ranjan.</h2>;
+  const three = <h3 className="big-heading">I build websites. I am specialised in backend.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a software engineer specializing in API development, deployment and website security..
+        Currently, I’m focused on building a bank at{' '}
+        <a href="https://nsave.com/" target="_blank" rel="noreferrer">
+          nsave.com
         </a>
         .
       </p>
