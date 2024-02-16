da3a49f21a97915d489ae69258bb9b5388ce443a
President <president@oxfordfls.org>
2024-02-17 04:45:06 +0530

    Update name and resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Reeshabh Ranjan',
+    description: 'Reeshabh is a software engineer who specializes in backend.',
+    siteUrl: 'https://reeshabh.in', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -20,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Reeshabh Ranjan',
+        short_name: 'Reeshabh Ranjan',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
