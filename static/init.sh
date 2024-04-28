3693278c9e1aa25818c2be3b52e7d766ecc56092
Reeshabh <reeshabhkumarranjan@gmail.com>
2024-04-28 19:00:01 +0530

    Update init.sh

--- static/init.sh
+++ static/init.sh
@@ -16,7 +16,7 @@ echo "Installing Docker"
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -28,13 +28,14 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
 echo "Installing fish"
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install fish
+sudo apt install -y fish
 fish_path=$(which fish)
 echo "Please edit /etc/passwd to change your shell to fish (${fish_path})"
